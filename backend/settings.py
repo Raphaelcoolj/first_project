d30bb5abc4f07621bacdd4e5bb81850f29b7a2bc
Ekeh Oghenerurie Chukwuemeka <ekehchukwuemeka375@gmail.com>
2025-11-07 22:50:23 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY
 SECRET_KEY = os.getenv("SECRET_KEY")  # from Render environment variables
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = False
 ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS", "crypto-backend-k32x.onrender.com")]
 
 # REST Framework
