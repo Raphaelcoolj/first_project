96021332001f88f0d547cfee68454c9833c3deae
Ekeh Oghenerurie Chukwuemeka <ekehchukwuemeka375@gmail.com>
2025-11-08 00:59:59 +0100

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -2,16 +2,18 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from .models import Wallet
 
-# Optional: extend User admin to show related wallet
+# Inline Wallet for User
 class WalletInline(admin.StackedInline):
     model = Wallet
     can_delete = False
     verbose_name_plural = 'Wallet'
+    readonly_fields = ('total_balance',)  # make total_balance read-only
 
+# Extend User admin
 class CustomUserAdmin(admin.ModelAdmin):
     inlines = (WalletInline,)
     list_display = ('username', 'email', 'is_staff', 'is_active')
 
+# Unregister default User and register new
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
-admin.site.register(Wallet)
